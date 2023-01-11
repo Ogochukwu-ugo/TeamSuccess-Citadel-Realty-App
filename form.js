4a59fcdfcf5600254cc2b87dc0294afba0263f7b
Ogochukwu Ugorji <ugorji.ogochukwu@gmail.com>
2023-01-11 13:25:14 +0100

    Added sign up validation

--- form.js
+++ form.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,38 +23,80 @@ const auth = getAuth();
 
 
 
+const email = document.getElementById('email');
+const password = document.getElementById('pswd');
+const firstName = document.getElementById('firstName');
+const form = document.getElementById("registers");
+const login = document.getElementById('login');
 
-    const email = document.getElementById('email ').value;
-        const password = document.getElementById(' pswd').value;
-        const firstName = document.getElementById('firstName ').value;
 
-const form = document.getElementById("form-detail");
-    form.addEventListener('submit',  (e) =>  {
-        e.preventDefault();
-        
-    
-        
-    
-        
-        createUserWithEmailAndPassword(auth, email, password, firstName)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
+//Sign Up validation
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+        // let user = { email: email.value, password: password.value, firstName: firstName.value }; Used to text if my form is receiving inputts.
+        // if(user.email === "" || user.password === "" || user.firstName === "") {
+        //     alert('Please input all required details');
+        // } else {
+        //     console.log(user);
+        // }
+        createUserWithEmailAndPassword(auth, email.value, password.value, firstName.value)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
 
-            
-            if(user && user !== null) {
+                if(user !== " " || user.isValid){
+
+                    set(ref(database, 'users/' + user.uid),{
+                        firstName: firstName,
+                        email: email
+                    });
+                    console.log("User created");
+                    window.location.href = './login.html';
+                }
                 
-                console.log(user);
-            }else{
-                console.log('error creating user');
-            }
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
 
-            console.log(errorMessage, errorCode);
-            // ..
-        });
+                console.log(errorMessage, errorCode);
+            });
+    
 });
+
+
+//Login validation
+
+login.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+
+        if(user !== " " || user.isValid){
+
+
+            const dt = new Date();
+            update(ref(database, 'users/' + user.uid),{
+                last_login: dt,
+            });
+            
+            window.location.href = './login.html';
+        }
+        //...
+        alert("Login successful");
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(errorCode, errorMessage);
+    });
+
+
+});
+
+
